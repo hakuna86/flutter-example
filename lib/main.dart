37139c8a1bf8851c9a3d420d1d3887580c35d52b
hakuna <kwangmin010@gmail.com>
2019-11-22 15:22:40 +0900

    make component

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,27 @@
 import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:my_app/components/grid.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+  static final showGrid = true; // Set to false to show ListView
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Box(
-          color: Colors.red,
-          width: 20,
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
         ),
-        Box(
-          color: Colors.amber,
-          width: 20,
-        ),
-        Box(
-          color: Colors.blueAccent,
-          width: 20,
-        ),
-        Box.Squear,
-      ],
-    );
-  }
-}
-
-class Box extends StatelessWidget {
-
-  static Box Squear = Box(
-    color: Colors.red,
-    width: 20,
-    height: 20,
-  );
-
-  Box({
-    Key key,
-    this.width = 100,
-    this.height = 100,
-    this.color = Colors.blue
-  }) : super(key: key);
-
-  final double width, height;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: this.width,
-      height: this.height,
-      decoration: BoxDecoration(
-        color: this.color,
-      ),
+        body: Center(
+          child: GridExam(
+            showGrid: false,
+          ),
+        )
+      )
     );
   }
 }
